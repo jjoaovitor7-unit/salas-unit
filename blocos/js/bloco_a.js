fd93fb2a561c4bce3e0af6ea79a4f4486a9b84ba
João Vítor S. F <48962164+jjoaovitor7@users.noreply.github.com>
2019-08-15 10:38:13 -0300

    Create bloco_a.js

---
+++ blocos/js/bloco_a.js
@@ -0,0 +1,159 @@
+function sala_a1(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a2(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a3(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a4(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a5(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a6(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a7(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a8(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a9(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a10(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a11(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a12(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a13(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a14(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a15(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a16(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a17(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a18(){
+  document.getElementById('sala').innerHTML= 'Laboratório (T.I)';
+}
+
+function sala_a19(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a20(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a21(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a22(){
+  document.getElementById('sala').innerHTML= 'Laboratório (T.I)';
+}
+
+function sala_a23(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a24(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a25(){
+  document.getElementById('sala').innerHTML= '?';
+ }
+
+function sala_a26(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a27(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a28(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a29(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a30(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a31(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a32(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a33(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a34(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a35(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a36(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a37(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a38(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a39(){
+  document.getElementById('sala').innerHTML= '?';
+}
+
+function sala_a40(){
+  document.getElementById('sala').innerHTML= '?';
+}
